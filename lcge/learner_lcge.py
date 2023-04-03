e6af55686113f548c193bba909642d4f8fe000ad
shouhulantian <237448659@qq.com>
2023-04-03 15:14:51 +0200

    hyper_search

--- lcge/learner_lcge.py
+++ lcge/learner_lcge.py
@@ -1,5 +1,6 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
 
+import os
 import argparse
 from typing import Dict
 import logging
@@ -17,194 +18,251 @@ import sys
 from regularizers_rule import RuleSim
 import json
 
-parser = argparse.ArgumentParser(
-    description="Logic and Commonsense-Guided Temporal KGE"
-)
-parser.add_argument(
-    '--dataset', type=str,
-    help="Dataset name"
-)
-models = [
-    'LCGE'
-]
-parser.add_argument(
-    '--model', choices=models,
-    help="Model in {}".format(models)
-)
-parser.add_argument(
-    '--max_epochs', default=50, type=int,
-    help="Number of epochs."
-)
-parser.add_argument(
-    '--valid_freq', default=5, type=int,
-    help="Number of epochs between each valid."
-)
-parser.add_argument(
-    '--rank', default=100, type=int,
-    help="Factorization rank."
-)
-parser.add_argument(
-    '--batch_size', default=1000, type=int,
-    help="Batch size."
-)
-parser.add_argument(
-    '--learning_rate', default=1e-1, type=float,
-    help="Learning rate"
-)
-parser.add_argument(
-    '--emb_reg', default=0., type=float,
-    help="Embedding regularizer strength"
-)
-parser.add_argument(
-    '--time_reg', default=0., type=float,
-    help="Timestamp regularizer strength"
-)
-parser.add_argument(
-    '--no_time_emb', default=False, action="store_true",
-    help="Use a specific embedding for non temporal relations"
-)
-parser.add_argument(
-    '--rule_reg', default=0., type=float,
-    help="Rule regularizer strength"
-)
-parser.add_argument(
-    '--weight_static', default=0., type=float,
-    help="Weight of static score"
-)
 
 
-args = parser.parse_args()
+def main(args):
+    weight_static_values = [0.0, 0.3, 0.6, 0.9, 1.2, 1.5]
 
-dataset = TemporalDataset(args.dataset)
+    best_global_mrr = 0.0
+    best_global_hit = 0.0
+    best_global_params = None
+    best_global_model_state = None
+    for weight_static in weight_static_values:
 
-with open("./src_data/rulelearning/" + args.dataset + "/rule1_p1.json", 'r') as load_rule1_p1:
-    rule1_p1 = json.load(load_rule1_p1)
-with open("./src_data/rulelearning/" + args.dataset + "/rule1_p2.json", 'r') as load_rule1_p2:
-    rule1_p2 = json.load(load_rule1_p2)
+        print(f"Training with weight_static: {weight_static}")
 
-f = open("./src_data/rulelearning/" + args.dataset + "/rule2_p1.txt", 'r')
-rule2_p1 = {}
-for line in f:
-    head, body1, body2, confi = line.strip().split("\t")
-    head, body1, body2, confi = int(head), int(body1), int(body2), float(confi)
-    if head not in rule2_p1:
-        rule2_p1[head] = {}
-    rule2_p1[head][(body1, body2)] = confi
-f.close()
+        args.weight_static = weight_static
 
-f = open("./src_data/rulelearning/" + args.dataset + "/rule2_p2.txt", 'r')
-rule2_p2 = {}
-for line in f:
-    head, body1, body2, confi = line.strip().split("\t")
-    head, body1, body2, confi = int(head), int(body1), int(body2), float(confi)
-    if head not in rule2_p2:
-        rule2_p2[head] = {}
-    rule2_p2[head][(body1, body2)] = confi
-f.close()
+        # traning begin
+        dataset = TemporalDataset(args.dataset)
 
-f = open("./src_data/rulelearning/" + args.dataset + "/rule2_p3.txt", 'r')
-rule2_p3 = {}
-for line in f:
-    head, body1, body2, confi = line.strip().split("\t")
-    head, body1, body2, confi = int(head), int(body1), int(body2), float(confi)
-    if head not in rule2_p3:
-        rule2_p3[head] = {}
-    rule2_p3[head][(body1, body2)] = confi
-f.close()
+        with open("./src_data/rulelearning/" + args.dataset + "/rule1_p1.json", 'r') as load_rule1_p1:
+            rule1_p1 = json.load(load_rule1_p1)
+        with open("./src_data/rulelearning/" + args.dataset + "/rule1_p2.json", 'r') as load_rule1_p2:
+            rule1_p2 = json.load(load_rule1_p2)
 
-f = open("./src_data/rulelearning/" + args.dataset + "/rule2_p4.txt", 'r')
-rule2_p4 = {}
-for line in f:
-    head, body1, body2, confi = line.strip().split("\t")
-    head, body1, body2, confi = int(head), int(body1), int(body2), float(confi)
-    if head not in rule2_p4:
-        rule2_p4[head] = {}
-    rule2_p4[head][(body1, body2)] = confi
-f.close()
+        f = open("./src_data/rulelearning/" + args.dataset + "/rule2_p1.txt", 'r')
+        rule2_p1 = {}
+        for line in f:
+            head, body1, body2, confi = line.strip().split("\t")
+            head, body1, body2, confi = int(head), int(body1), int(body2), float(confi)
+            if head not in rule2_p1:
+                rule2_p1[head] = {}
+            rule2_p1[head][(body1, body2)] = confi
+        f.close()
 
-rules = (rule1_p1, rule1_p2, rule2_p1, rule2_p2, rule2_p3, rule2_p4)
+        f = open("./src_data/rulelearning/" + args.dataset + "/rule2_p2.txt", 'r')
+        rule2_p2 = {}
+        for line in f:
+            head, body1, body2, confi = line.strip().split("\t")
+            head, body1, body2, confi = int(head), int(body1), int(body2), float(confi)
+            if head not in rule2_p2:
+                rule2_p2[head] = {}
+            rule2_p2[head][(body1, body2)] = confi
+        f.close()
 
-sizes = dataset.get_shape()
-print("sizes of dataset is:\t", sizes)
-model = {
-    'LCGE': LCGE(sizes, args.rank, rules, args.weight_static, no_time_emb=args.no_time_emb),
-}[args.model]
-model = model.to('cuda' if torch.cuda.is_available() else 'cpu')
+        f = open("./src_data/rulelearning/" + args.dataset + "/rule2_p3.txt", 'r')
+        rule2_p3 = {}
+        for line in f:
+            head, body1, body2, confi = line.strip().split("\t")
+            head, body1, body2, confi = int(head), int(body1), int(body2), float(confi)
+            if head not in rule2_p3:
+                rule2_p3[head] = {}
+            rule2_p3[head][(body1, body2)] = confi
+        f.close()
 
+        f = open("./src_data/rulelearning/" + args.dataset + "/rule2_p4.txt", 'r')
+        rule2_p4 = {}
+        for line in f:
+            head, body1, body2, confi = line.strip().split("\t")
+            head, body1, body2, confi = int(head), int(body1), int(body2), float(confi)
+            if head not in rule2_p4:
+                rule2_p4[head] = {}
+            rule2_p4[head][(body1, body2)] = confi
+        f.close()
 
-opt = optim.Adagrad(model.parameters(), lr=args.learning_rate)
+        rules = (rule1_p1, rule1_p2, rule2_p1, rule2_p2, rule2_p3, rule2_p4)
 
-emb_reg = N3(args.emb_reg)
-time_reg = Lambda3(args.time_reg)
-rule_reg = RuleSim(args.rule_reg)       # relation embedding reglu via rules
+        sizes = dataset.get_shape()
+        print("sizes of dataset is:\t", sizes)
+        model = {
+            'LCGE': LCGE(sizes, args.rank, rules, args.weight_static, no_time_emb=args.no_time_emb),
+        }[args.model]
+        model = model.cuda()
 
-best_mrr = 0.
-best_hit = 0.
-early_stopping = 0
+        opt = optim.Adagrad(model.parameters(), lr=args.learning_rate)
 
-for epoch in range(args.max_epochs):
-    examples = torch.from_numpy(
-        dataset.get_train().astype('int64')
-    )
-    #print("\nexamples:\n", examples.size())
+        emb_reg = N3(args.emb_reg)
+        time_reg = Lambda3(args.time_reg)
+        rule_reg = RuleSim(args.rule_reg)  # relation embedding reglu via rules
 
-    model.train()
-    if dataset.has_intervals():
-        optimizer = IKBCOptimizer(
-            model, emb_reg, time_reg, opt, dataset,
-            batch_size=args.batch_size
-        )
-        optimizer.epoch(examples)
+        best_mrr = 0.
+        best_hit = 0.
+        early_stopping = 0
 
-    else:
-        optimizer = TKBCOptimizer(
-            model, emb_reg, time_reg, rule_reg, opt,
-            batch_size=args.batch_size
-        )
-        optimizer.epoch(examples)
+        for epoch in range(args.max_epochs):
+            examples = torch.from_numpy(
+                dataset.get_train().astype('int64')
+            )
+            # print("\nexamples:\n", examples.size())
 
+            model.train()
+            if dataset.has_intervals():
+                optimizer = IKBCOptimizer(
+                    model, emb_reg, time_reg, opt, dataset,
+                    batch_size=args.batch_size
+                )
+                optimizer.epoch(examples)
 
-    def avg_both(mrrs: Dict[str, float], hits: Dict[str, torch.FloatTensor]):
-        """
-        aggregate metrics for missing lhs and rhs
-        :param mrrs: d
-        :param hits:
-        :return:
-        """
-        m = (mrrs['lhs'] + mrrs['rhs']) / 2.
-        h = (hits['lhs'] + hits['rhs']) / 2.
-        return {'MRR': m, 'hits@[1,3,10]': h}
-
-    if epoch < 0 or (epoch + 1) % args.valid_freq == 0:
-        if dataset.has_intervals():
-            valid, test, train = [
-                dataset.eval(model, split, -1 if split != 'train' else 50000)
-                for split in ['valid', 'test', 'train']
-            ]
-            print("valid: ", valid)
-            print("test: ", test)
-            print("train: ", train)
-
-        else:
-            valid, test, train = [
-                avg_both(*dataset.eval(model, split, -1 if split != 'train' else 50000))
-                for split in ['valid', 'test', 'train']
-            ]
-            print("epoch: ", epoch+1)
-            print("valid: ", valid['MRR'])
-            print("test: ", test['MRR'])
-            print("train: ", train['MRR'])
-
-            print("test hits@n:\t", test['hits@[1,3,10]'])
-            if test['MRR'] > best_mrr:
-                best_mrr = test['MRR']
-                best_hit = test['hits@[1,3,10]']
-                early_stopping = 0
             else:
-                early_stopping += 1
-            if early_stopping > 10:
-                print("early stopping!")
-                break
+                optimizer = TKBCOptimizer(
+                    model, emb_reg, time_reg, rule_reg, opt,
+                    batch_size=args.batch_size
+                )
+                optimizer.epoch(examples)
 
-print("The best test mrr is:\t", best_mrr)
-print("The best test hits@1,3,10 are:\t", best_hit)
+            def avg_both(mrrs: Dict[str, float], hits: Dict[str, torch.FloatTensor]):
+                """
+                aggregate metrics for missing lhs and rhs
+                :param mrrs: d
+                :param hits:
+                :return:
+                """
+                m = (mrrs['lhs'] + mrrs['rhs']) / 2.
+                h = (hits['lhs'] + hits['rhs']) / 2.
+                return {'MRR': m, 'hits@[1,3,10]': h}
+
+            if epoch < 0 or (epoch + 1) % args.valid_freq == 0:
+                if dataset.has_intervals():
+                    valid, test, train = [
+                        dataset.eval(model, split, -1 if split != 'train' else 50000)
+                        for split in ['valid', 'test', 'train']
+                    ]
+                    print("valid: ", valid)
+                    print("test: ", test)
+                    print("train: ", train)
+
+                else:
+                    valid, test, train = [
+                        avg_both(*dataset.eval(model, split, -1 if split != 'train' else 50000))
+                        for split in ['valid', 'test', 'train']
+                    ]
+                    print("epoch: ", epoch + 1)
+                    print("valid: ", valid['MRR'])
+                    print("test: ", test['MRR'])
+                    print("train: ", train['MRR'])
+
+                    print("test hits@n:\t", test['hits@[1,3,10]'])
+                    if test['MRR'] > best_mrr:
+                        best_mrr = test['MRR']
+                        best_hit = test['hits@[1,3,10]']
+                        early_stopping = 0
+                    else:
+                        early_stopping += 1
+                    if early_stopping > 10:
+                        print("early stopping!")
+                        break
+                if test['MRR'] > best_global_mrr:
+                    best_global_mrr = test['MRR']
+                    best_global_hit = test['hits@[1,3,10]']
+                    best_global_params = {
+                        'weight_static': weight_static,
+                    }
+                    best_global_model_state = model.state_dict()
+        print("The best test mrr is:\t", best_mrr)
+        print("The best test hits@1,3,10 are:\t", best_hit)
+
+    # save optimal model
+    output_dir = "best_model"
+    os.makedirs(output_dir, exist_ok=True)
+
+    torch.save(
+        {
+            "model_state_dict": best_global_model_state,
+            "params": best_global_params,
+            "best_mrr": best_global_mrr,
+            "best_hit": best_global_hit,
+        },
+        os.path.join(output_dir, "best_model.pt"),
+    )
+
+    print("The best model and parameters have been saved.")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Logic and Commonsense-Guided Temporal KGE"
+    )
+    parser.add_argument(
+        '--dataset', default='ICEWS14', type=str,
+        help="Dataset name"
+    )
+    models = [
+        'LCGE'
+    ]
+    parser.add_argument(
+        '--model', default='LCGE', choices=models,
+        help="Model in {}".format(models)
+    )
+    parser.add_argument(
+        '--max_epochs', default=50, type=int,
+        help="Number of epochs."
+    )
+    parser.add_argument(
+        '--valid_freq', default=5, type=int,
+        help="Number of epochs between each valid."
+    )
+    parser.add_argument(
+        '--rank', default=100, type=int,
+        help="Factorization rank."
+    )
+    parser.add_argument(
+        '--batch_size', default=1000, type=int,
+        help="Batch size."
+    )
+    parser.add_argument(
+        '--learning_rate', default=1e-1, type=float,
+        help="Learning rate"
+    )
+    parser.add_argument(
+        '--emb_reg', default=0., type=float,
+        help="Embedding regularizer strength"
+    )
+    parser.add_argument(
+        '--time_reg', default=0., type=float,
+        help="Timestamp regularizer strength"
+    )
+    parser.add_argument(
+        '--no_time_emb', default=False, action="store_true",
+        help="Use a specific embedding for non temporal relations"
+    )
+    parser.add_argument(
+        '--rule_reg', default=0., type=float,
+        help="Rule regularizer strength"
+    )
+
+    parser.add_argument(
+        '--weight_static', type=float,
+        help="Weight of static score"
+    )
+
+    args, unknown = parser.parse_known_args()
+
+    main(args)
+
+
+# load best model
+"""
+checkpoint = torch.load("best_model/best_model.pt")
+model.load_state_dict(checkpoint["model_state_dict"])
+best_params = checkpoint["params"]
+best_mrr = checkpoint["best_mrr"]
+best_hit = checkpoint["best_hit"]
+
+print("Loaded best model with parameters:")
+print(best_params)
+print(f"Best MRR: {best_mrr}")
+print(f"Best Hit: {best_hit}")
+
+"""
+
